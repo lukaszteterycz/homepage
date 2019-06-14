41ee5ea908a25e9b8f899aa9a9d01763ed2ee197
Łukasz Teterycz <lukasz@graftet.pl>
2019-06-14 20:47:03 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log("siemanko 👋🏿");
+const name = "Łukasz";
+const age = 29;
+console.log(name);
+console.log(age);
+console.log(`${name}
+${age}`);
+console.log(`Nazywam się ${name} 
+i mam ${age} lat`);
+
+console.log("Nazywam się " + name + " i mam " + age + " lat");
