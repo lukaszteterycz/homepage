36f3b3c5fc065c6bd8d5d8383036a16f20ac89a2
Łukasz Teterycz <lukasz@graftet.pl>
2019-06-17 17:34:53 +0200

    refactoring bem

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ console.log(`Nazywam się ${name}
 i mam ${age} lat`);
 
 console.log("Nazywam się " + name + " i mam " + age + " lat");
+
+const about = document.querySelector(".about__paragraph--js");
+console.log(about.innerHTML);
+
+about.innerHTML = `<strong>Nazywam się</strong> ${name} i mam ${age} lat`;
+
+const paragraphs = document.querySelectorAll("p");
+
+console.log(paragraphs);
+console.log(paragraphs[3]);
