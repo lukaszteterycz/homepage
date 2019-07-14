57d313c32e97b6723f4438fd82d0868ad693ad58
Łukasz Teterycz <lukasz@graftet.pl>
2019-07-14 14:42:11 +0200

    add hamburger menu and update grid

--- main.js
+++ main.js
@@ -74,3 +74,18 @@ const myName = 'lukas';
 const myAge = 29;
 
 welcome(myName, myAge);
+
+// HAMBURGER MENU, DODANIE KLASY
+const navigationSwitcher = document.querySelector('.navigation__switcher--js');
+
+navigationSwitcher.addEventListener('click', (e) => {
+  const navigationList = document.querySelector('.navigation__list--js');
+  navigationList.classList.toggle('navigation__list--visible');
+  if (navigationList.classList.contains('navigation__list--visible')) {
+    navigationSwitcher.innerHTML = 'X';
+  } else {
+    navigationSwitcher.innerHTML = '☰';
+  }
+});
+
+
